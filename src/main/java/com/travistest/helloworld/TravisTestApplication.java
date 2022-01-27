ba9a461571203b88feb789d2b94c9d53d9d56887
totw5701 <76473868+totw5701@users.noreply.github.com>
2022-01-27 15:06:51 +0900

    임시 프로젝트 구성

---
+++ src/main/java/com/travistest/helloworld/TravisTestApplication.java
@@ -0,0 +1,13 @@
+package com.travistest.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravisTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/travistest/helloworld/TravisTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travistest.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
